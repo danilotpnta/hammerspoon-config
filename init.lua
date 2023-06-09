476d03a61fdcd2870dd17e5542ceefa737d7389e
Danilo Toapanta <70687643+danilotpnta@users.noreply.github.com>
2023-06-09 22:11:36 +0200

    reformat

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Autoreload Configuration
 function reloadConfig(files)
     doReload = false
     for _, file in pairs(files) do
@@ -10,13 +11,9 @@ function reloadConfig(files)
     end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-
---hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(paths)
---    reloadConfig(paths) end):start()
-
 hs.alert.show("New changes applied")
 
---
+--- Open App
 function open(name)
     return function()
         hs.application.launchOrFocus(name)
@@ -26,50 +23,33 @@ function open(name)
     end
 end
 
-hs.hotkey.bind({ "cmd" }, "E", open("Finder"))
-hs.hotkey.bind({ "cmd" }, "Y", open("Youtube"))
-hs.hotkey.bind({ "cmd" }, "T", open("Google Translate"))
-hs.hotkey.bind({ "cmd", "alt" }, "D", open("Google Drive Space Society Twente"))
-hs.hotkey.bind({ "cmd", }, "D", open("Google Drive"))
-hs.hotkey.bind({ "cmd" }, "2", open("Gmail Danilo"))
-hs.hotkey.bind({ "cmd" }, "3", open("Microsoft Outlook"))
-hs.hotkey.bind({ "cmd" }, "4", open("Gmail Fatema"))
-hs.hotkey.bind({ "cmd" }, "K", open("Google Keep"))
-hs.hotkey.bind({ "cmd" }, "M", open("Google Maps"))
-hs.hotkey.bind({ "cmd", "option" }, "C", open("Google Calendar"))
-hs.hotkey.bind({ "cmd", "option", "shift" }, "C", open("Google Calendar Fatema"))
-hs.hotkey.bind({ "cmd" }, "W", open("WhatsApp"))
-
--- Focus only in Edge
---local wf=hs.window.filter
---wf:setAppFilter(appname,false)
---
---
-
---
+--- Open Microsoft Edge Tab
 function open_NewTab(name)
-    --hs.appfinder.appFromName("Microsoft Edge"):activate()
-
     return function()
-        local curr_win_name = hs.application.frontmostApplication():name()
         local browser = hs.appfinder.appFromName("Microsoft Edge")
         local str_menu_item = { "File", "New Tab" }
-
-
-        --if ppl == "Open_NewTab" then
-        --    str_menu_item = { "File", "New Tab" }
-        --end
-
         local menu_item = browser:findMenuItem(str_menu_item)
         if (menu_item) then
             browser:selectMenuItem(str_menu_item)
         end
-        if curr_win_name:sub(-14) ~= "Microsoft Edge" then
-            hs.application.frontmostApplication():hide()
-        end
 
+        hs.timer.doAfter(0.001, function()
+            local curr_win_name = hs.application.frontmostApplication():name()
+            if curr_win_name:sub(-14) ~= "Microsoft Edge" then
+                hs.application.frontmostApplication():hide()
+            end
+        end)
     end
 end
 
---- open different Microsoft Edge users
+hs.hotkey.bind({ "cmd" }, "E", open("Finder"))
+hs.hotkey.bind({ "cmd" }, "Y", open("Youtube"))
+hs.hotkey.bind({ "cmd" }, "T", open("Google Translate"))
+hs.hotkey.bind({ "cmd", }, "D", open("Google Drive"))
+hs.hotkey.bind({ "cmd" }, "2", open("Gmail Danilo"))
+hs.hotkey.bind({ "cmd" }, "3", open("Microsoft Outlook"))
+hs.hotkey.bind({ "cmd" }, "K", open("Google Keep"))
+hs.hotkey.bind({ "cmd" }, "M", open("Google Maps"))
+hs.hotkey.bind({ "cmd", "option" }, "C", open("Google Calendar"))
+hs.hotkey.bind({ "cmd" }, "W", open("WhatsApp"))
 hs.hotkey.bind({ "cmd" }, "X", open_NewTab("Microsoft Edge"))
